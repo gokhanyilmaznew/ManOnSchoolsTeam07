fedb2cf8f0553f3374a766b4caaa98f376469100
gokhanyilmaznew <gokhan.isbirlikleri@gmail.com>
2023-09-14 18:37:28 +0300

    Okul projesi Team7

---
+++ ManOnSchoolsTeam07/src/test/java/base_url/ManagementSchoolBaseUrl.java
@@ -0,0 +1,16 @@
+package base_url;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static utilities.Authentication.generateToken;
+
+public class ManagementSchoolBaseUrl {
+    public static RequestSpecification spec;
+    public static void setup(String username, String password){
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).
+                addHeader("Authorization", generateToken(username,password)).
+                setBaseUri(ConfigReader.getProperty("base_url")).build();
+    }
+}
